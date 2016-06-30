d3d82ee35f9f3415db97580d454dee37712a57b0
Sanjeev Budha <budhasanjeev@gmail.com>
2016-06-30 13:01:35 +0545

    updated

---
+++ controller/auth.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sanjeevbudha
+ * Date: 6/30/16
+ * Time: 12:32 PM
+ */
+require("../config/databaseConnection.php");
+require("../common/Common.php");
+
+session_start();
+
+if(isset($_POST['login'])){
+
+    $objCommon = new Common();
+
+    $email = addslashes($_POST['email']);
+    $password = addslashes($_POST['password']);
+
+    $result = array();
+
+    $result = $objCommon->login($connection,$email,$password);
+
+    if($result['message']=='success'){
+        header('Location:../views/dashboard.php');
+    }
+    else{
+        header('Location:../views/login.php');
+    }
+}
+else{
+
+    echo "<script>alert('hello')</script>";
+}
