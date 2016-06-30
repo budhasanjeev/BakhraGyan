b1bdabec9720b5c96f9c7d07356dd79cc0c25207
Sanjeev Budha <budhasanjeev@gmail.com>
2016-07-01 02:09:30 +0545

    updated

--- controller/foodHandler.php
+++ controller/foodHandler.php
@@ -31,25 +31,18 @@ if($_POST["mode"]=="add"){
     echo $result;
 }
 
-if($_POST["mode"]=="edit"){
+else if($_POST["mode"]=="edit"){
 
-    $id = $_POST['user_id'];
-    $news_headline = $_POST['newsHeadline'];
-    $news_body = $_POST['newsBody'];
-    $category = $_POST['category'];
+    $id = $_POST['id'];
 
-    $result = $objCommon->updateNews($news_headline,$news_body,$category,$id);
+    $result = array();
+    $result = $objCommon->editFood($connection,$id);
 
-    if($result){
-        $_SESSION['edit'] ="success";
-    } else {
-        $_SESSION['edit'] ="error";
-    }
+    echo json_encode($result);
 
-    header('Location:../views/news.php');
 }
 
-if($_POST["mode"]=="delete"){
+else if($_POST["mode"]=="delete"){
 
     $id = $_POST['id'];
 
@@ -58,4 +51,26 @@ if($_POST["mode"]=="delete"){
     $result = $objCommon->deleteFood($connection,$id);
 
     echo json_encode($result);
+}
+
+else if($_POST['mode']=="update"){
+
+    $f_id = $_POST['food_id'];
+    $food_name = $_POST['feedName'];
+    $description = $_POST['description'];
+
+    $image = $_FILES['image']['name'];
+    $image_tmp = $_FILES['image']['tmp_name'];
+
+    move_uploaded_file($image_tmp,"../images/$image");
+
+    $result = array();
+    $result = $objCommon->updateFood($connection,$food_name,$description,$image,$f_id);
+
+    if($result['message']=='success'){
+        header("Location:../views/food.php");
+    }
+
+    echo json_encode($result);
+
 }
