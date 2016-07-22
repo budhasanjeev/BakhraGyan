f99f2d49fca43d8013448d412ae3a94c771563fa
Arun Tamang <arun.tamang@deerwalk.edu.np>
2016-07-22 18:09:05 +0545

    create shed page

--- views/shed.php
+++ views/shed.php
@@ -24,138 +24,120 @@ require('../views/Layout/header.php');
 ?>
 
 <div id="content">
-    <button class="btn btn-primary" id="add-breed"> खोर बारेमा थप्नुहोस  </button>
+    <button class="btn btn-primary" id="add-shed"> खोर बारेमा थप्नुहोस  </button>
     <?php
 
-    $userList = array();
+    $shedList = array();
     $objCommon = new Common();
-    $userList = $objCommon->getBreed($connection);
-
-
-    echo '
-                <table class="table table-striped table-responsive">
-                        <thead>
-                            <tr>
-                                <th> विवरण </th>
-                                <th>फोटो</th>
-
-                            </tr>
-                        </thead>
-
-                        <tbody>';
-
-    foreach($userList as $user){
-
-        if($user['status']==0){
-            $status = "Inactive";
-        }
-        else {
-            $status = "Active";
-        }
-
-        echo '
-                    <tr>
-                        <td><img class="img-circle" src="../images/profile_pictures/' .$user["profile_picture"].'" style="height: 70px;width:70px;" onclick="profileView('.$user["id"].')"></td>
-                        <td style="vertical-align: middle;">'.$user["first_name"].'&nbsp;'.$user["last_name"].'</td>
-                        <td style="vertical-align: middle;">'.$user["mobile_number"].'</td>
-                        <td style="vertical-align: middle;">'.$user["phone_number"].'</td>
-                        <td style="vertical-align: middle;">'.$user["email_address"].'</td>
-                        <td style="vertical-align: middle;">'.$user["role"].'</td>
-                        <td style="vertical-align: middle;">'.$status.'</td>
-                    </tr>
-
-                ';
-    }
-
-    echo'
-                    </tbody>
-                </table>
-            ';
-
+    $shedList = $objCommon->getShed();
     ?>
+    <table class="table table-striped table-responsive" id="breed-table">
+        <thead>
+        <tr>
+            <th>फोटो</th>
+            <th>शीर्षक </th>
+            <th>विवरण</th>
+            <th>कार्यहरू</th>
+        </tr>
+        </thead>
+
+
+    <tbody>
+        <?php
+        foreach($shedList as $shed){?>
+
+
+            <tr>
+                <td><img class="img-circle" src="../images/<?php echo $shed["photo"] ?>" style="height: 70px;width:70px;">
+                <td style="vertical-align: middle;"><?php echo $shed["title"]?></td>
+                <td style="vertical-align: middle;"><?php echo $shed["description"]?></td>
+                <td style="vertical-align: middle;">
+                    <button class="btn btn-danger" onclick="return deleteShed(<?php echo $shed['id']?>)"><span class="glyphicon glyphicon-trash"></span></button>
+                    <button class="btn btn-success" onclick="return editShed(<?php echo $shed['id'] ?>)"><span class="glyphicon glyphicon-edit"></span></button>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
 </div>
 
 <?php
 require('../views/Layout/footer.php');
 ?>
 
-<div class="modal fade" id="view_profile" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
-    <div class="modal-dialog" style="width: auto">
+<div class="modal fade" id="insert-shed" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
         <div class="modal-content">
 
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h3 class="modal-title"></h3>
+                <h2 class="modal-title"></h2>
             </div>
 
             <div class="modal-body">
 
-                <div class="container">
-
-                    <div class="col-md-3" style="border-right: 1px solid #226CB5;" data-spy="affix" data-offset-top="60" data-offset-bottom="200">
-                        <div class="row profile-pic">
-                            <img style="width: 60%; height: 40%" src="">
-                        </div>
-                        <div class="row profile-table">
-                            <h2 id="fullName"></h2>
-                            <hr/>
-                            <table cellpadding="1">
-                                <tr>
-                                    <td><span class="glyphicon glyphicon-home"></span></td>
-                                    <td><h5 id="address"></h5></td>
-                                </tr>
-
-                                <tr>
-                                    <td><span class="glyphicon glyphicon-earphone"></span></td>
-                                    <td><h5 id="mobileNumber"></h5></td>
-                                </tr>
-
-                                <tr>
-                                    <td><span class="glyphicon glyphicon-phone-alt"></span></td>
-                                    <td><h5 id="phoneNumber"></h5></td>
-                                </tr>
-
-                                <tr>
-                                    <td><span class="glyphicon glyphicon-envelope"></span></td>
-                                    <td><h5 id="emailAddress"></h5></td>
-                                </tr>
-                            </table>
+                <form class="form-horizontal" role="form" id="shed-form" method="post" action="" enctype="multipart/form-data">
+                    <input type="hidden" name="mode" id="mode">
+                    <input type="hidden" name="shed_id" id="shed_id">
+                    <div class="form-group">
+                        <label class="control-label col-sm-4" for="shedTitle">Title</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="shedTitle" name="shedTitle">
                         </div>
                     </div>
 
-                    <div class="col-md-8" style="float: right;">
+                    <div class="form-group">
+                        <label class="control-label col-sm-4" for="description"> Description </label>
 
-                        <div class="tab-content">
-                            <div id="history" class="tab-pane fade in active">
-                                <h3>News History</h3>
-                                <table id="newsList" style="text-align: left;" class="table table-bordered table-responsive">
-                                    <thead>
-                                    <tr>
-                                        <td>S.N</td>
-                                        <th>News headline</th>
-                                        <th>Created Date</th>
-                                        <th>Last Updated Date</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody id="newsListBody">
-
-                                    </tbody>
-                                </table>
-                            </div>
+                        <div class="col-sm-8">
+                            <textarea type="text" class="form-control" id="description" name="description" style="width: 100%;height:250px"></textarea>
                         </div>
                     </div>
 
-                </div>
+                    <div id="shed-img" class="form-group">
+                        <label class="control-label col-sm-4" for="image">image</label>
+
+                        <input type="file" id="image" name="image" >
+                    </div>
+
+                    <div class="form-group">
+                        <label  class="control-label col-sm-4"></label>
+                        <button type="submit" class="btn btn-default" id="save-shed"></button>
+                    </div>
+                </form>
 
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
 
         </div>
     </div>
 </div>
+
+<script>
+    $('#add-shed').on('click',function(){
+
+        $('#insert-shed').modal('show');
+        $('#insert-shed .modal-title').html("Shed");
+        $('#insert-shed button[type=submit]').html("Add");
+        $('#shed-form').attr('action','../controller/shedHandler.php');
+        $('#mode').attr('value','add');
+
+    })
+
+
+    $(document).ready(function(){
+        $('#cure-table').DataTable({
+            "info":true,
+            "paging":true,
+            "ordering":false,
+            "lengthMenu":[[2,4,6,-1],[2,4,6,"All"]]
+        })
+    })
+</script>
 </body>
 </html>
 
