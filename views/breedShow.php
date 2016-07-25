4a0ffbed2ae90491712c02c73aa149c3bceb31ba
Arun Tamang <arun.tamang@deerwalk.edu.np>
2016-07-25 15:53:52 +0545

    create show pages

---
+++ views/breedShow.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arun Tamang
+ * Date: 7/25/2016
+ * Time: 3:27 PM
+ */
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>बाख्रा ज्ञान </title>
+</head>
+
+    <body>
+        <?php
+        require('../views/Layout/header.php');
+        ?>
+
+
+        <?php
+        require('../views/Layout/footer.php');
+        ?>
+    </body>
+</html>
---
+++ views/foodShow.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arun Tamang
+ * Date: 7/25/2016
+ * Time: 3:27 PM
+ */
