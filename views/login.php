0afb3c1802d595113b5368bbb32d6102634fa87e
Arun Tamang <arun.tamang@deerwalk.edu.np>
2016-08-29 17:44:47 +0545

    updated

--- views/login.php
+++ views/login.php
@@ -47,7 +47,7 @@
 <div class="container" style="width: 60%;margin-top: 5%">
 
     <fieldset>
-    <legend style="margin-left: 10%">लग-इन गारनुहोस</legend>
+    <legend style="margin-left: 10%">लग-इन  गर्नुहोस्</legend>
     <form class="form-horizontal" role="form" id="login-form" action="../controller/auth.php" method="post">
 
         <input type="hidden" name="login" value="login">
